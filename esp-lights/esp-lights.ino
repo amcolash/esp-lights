ae34af0e410e407ec528714df420b0b9a4da06f1
Andrew McOlash <amcolash@gmail.com>
2022-05-09 02:05:53 -0700

    arduino

---
+++ esp-lights/esp-lights.ino
@@ -0,0 +1,71 @@
+#include <creds.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#define ON_BUTTON D5
+#define OFF_BUTTON D7
+
+int state = -1;
+
+void setupSerial() {
+  Serial.begin(115200);
+
+  delay(200);
+  Serial.println("\n-----------------");
+  Serial.println("Turning On");
+}
+
+
+void IRAM_ATTR turnOn() { state = 1; }
+void IRAM_ATTR turnOff() { state = 0; }
+
+void setupPins() {
+  pinMode(ON_BUTTON, INPUT_PULLUP);
+  pinMode(OFF_BUTTON, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(ON_BUTTON), turnOn, FALLING);
+  attachInterrupt(digitalPinToInterrupt(OFF_BUTTON), turnOff, FALLING);
+}
+
+void setupWifi() {
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println();
+  Serial.print("Connected, IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void updateState() {
+  Serial.println("Sending data to server [" + String(state) + "]");
+
+  WiFiClient wifiClient;
+  HTTPClient http;
+
+  if (state == 1) http.begin(wifiClient, String(SERVER) + "/on");
+  else http.begin(wifiClient, String(SERVER) + "/off");
+
+  http.GET();
+  http.end();
+
+  state = -1;
+}
+
+void setup() {
+  setupSerial();
+  setupPins();
+  setupWifi();
+}
+
+void loop() {
+  if (state != -1) {
+    updateState();
+
+    // Wait for a moment just in case
+    delay(1000);
+  }
+}
